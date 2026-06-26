84063f2f5689456367f7d99abea7e478c988e3ad
Zzrispu <jarturoliveira30@gmail.com>
2026-06-26 11:19:12 -0300

    começo de UI

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,6 @@ const finalStates = ref<State[]>([]);
 const auxiliaryAlphabet = ref<AuxiliaryAlphabet>(["*", "X", "B"]);
 
 const tape = ref<string>(auxiliaryAlphabet.value[0] + word.value + auxiliaryAlphabet.value[2]);
-watch(word, (newWord) => {
-  tape.value = auxiliaryAlphabet.value[0] + newWord + auxiliaryAlphabet.value[2];
-  head.value = 1;
-});
 
 class State {
   private _label: string;
@@ -66,15 +62,37 @@ class State {
   }
 }
 
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const tapeRef = ref<HTMLDivElement | null>(null);
+
+function moveTape() {
+  if (!tapeRef.value || !tapeRef.value.children.length) return;
+    
+  const tapeSlotsLength = Array.from(tapeRef.value.children).map((slot) => slot.getBoundingClientRect().width);
+  tapeRef.value.style.transform = `translateX(${
+    (tapeRef.value.getBoundingClientRect().width / 2) - tapeSlotsLength.slice(0, head.value).reduce((sum, value) => sum + value, 0) - (tapeSlotsLength.at(head.value) as number / 2)
+  }px`;
+}
+
 function iterate() {
   try {
     currentState.value = currentState.value?.executeTransition();
+    moveTape();
   } catch (error) {
     console.error(error);
   }
 }
 
 onMounted(() => {
+  // A função anonima abeixo executa uma vez e não gera reziduos
+  (() => {
+    const { width: canvaWidth, height: canvaHeight } = (canvasRef.value?.parentElement as HTMLElement).getBoundingClientRect();
+    canvasRef.value!.width = canvaWidth;
+    canvasRef.value!.height = canvaHeight;
+
+    moveTape();
+  })();
+
   const [startSymbol, markSymbol, blankSymbol] = auxiliaryAlphabet.value;
 
   const q0 = new State();
@@ -111,34 +129,94 @@ onMounted(() => {
 
   currentState.value = q0;
   finalStates.value = [q6];
+
+  // Quando o input da palavra muda, essa função é chamada
+  watch(word, (newWord) => {
+    tape.value = auxiliaryAlphabet.value[0] + newWord + auxiliaryAlphabet.value[2];
+    head.value = 1;
+    currentState.value = q0;
+  });
 })
 </script>
 
 <template>
-  <h1>Máquina de Turing</h1>
-  <label for="i-word">Palavra:</label>
-  <input id="i-word" v-model="word" />
-  <button @click="iterate">▶</button>
-  <p>Alfabeto: { {{ [...new Set(word)].join(", ") }} }</p>
-  <p>Alfabeto Auxiliar: { {{ auxiliaryAlphabet.join(", ") }} }</p>
-  <p>Estados Finais: {{ finalStates.map(s => s.label).join(", ") }}</p>
-  <p>Estado Atual: {{ currentState?.label }}</p>
-  <div class="tape">
-   <div v-for="(char, index) in tape" :key="index" :class="{ head: index === head }">
-      {{ char }}
-    </div>
-  </div>
+  <main>
+    <section id="machine-config-area" style="grid-area: machine-config-area; border: 1px solid blue;">
+      <h1>Máquina de Turing</h1>
+      <label for="i-word">Palavra:</label>
+      <input id="i-word" v-model="word" />
+      <button @click="iterate">▶</button>
+      <p>Alfabeto: { {{ [...new Set(word)].join(", ") }} }</p>
+      <p>Alfabeto Auxiliar: { {{ auxiliaryAlphabet.join(", ") }} }</p>
+      <p>Estados Finais: {{ finalStates.map(s => s.label).join(", ") }}</p>
+      <p>Estado Atual: {{ currentState?.label }}</p>
+    </section>
+    <section id="canva-area" style="grid-area: canva-area; border: 1px solid black;">
+      <canvas id="canva" ref="canvasRef"></canvas>
+    </section>
+    <section id="machine-area" style="grid-area: machine-area; border: 1px solid yellow;">
+      <svg width="24px" height="24px" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon points="0,0 100,0 50,100" style="stroke:red; fill: transparent;" />
+      </svg>
+      <div class="tape" ref="tapeRef">
+        <div v-for="(char, index) in tape" :key="index" :class="{ head: index === head }">
+          {{ char }}
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-.tape {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
+main {
+  min-height: 100vh;
+  max-height: 100vh;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 1fr auto;
+  grid-template-areas:
+    "machine-config-area canva-area"
+    "machine-config-area machine-area";
 }
 
-.head {
-  background-color: yellow;
+#machine-config-area, #machine-area {
+  padding: 1rem;
+}
+
+#canva-area {
+  min-height: 0;
+  min-width: 0;
+  overflow: hidden;
+  display: grid;
+
+  &>#canva {
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+}
+
+#machine-area {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+
+  &>svg {
+    width: 3rem;
+    height: 4rem;
+  }
+
+  .tape {
+    display: flex;
+    border: 1px solid lime;
+    font-size: 4rem;
+    margin-bottom: 2rem;
+    transition: transform 0.1s ease;
+  }
+
+  .head {
+    background-color: yellow;
+  }
 }
 </style>
